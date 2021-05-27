a176496bd25e9c7e0890b41616c07e01e3725973
unknown <thanhpham.23102000@gmail.com>
2021-05-27 16:44:28 +0700

    update file delelt

--- film/src/app/controllers/DeleteController.js
+++ film/src/app/controllers/DeleteController.js
@@ -1,5 +1,5 @@
 const films = require('../models/films');
-const { mutipleMongooseToObject} = require('../../util/mongoose');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 class deleteController {
     delete(req, res, next) {
         films.find({})
@@ -10,10 +10,18 @@ class deleteController {
             })
             .catch(next)
     }
-    delete_post(req, res, next){
+    delete_post(req, res, next) {
         films.find({})
-            .then(films =>{
+            .then(films => {
                 return res.json(mutipleMongooseToObject(films));
+            })
+            .catch(next)
+    }
+    deleteByID(req, res, next) {
+        var id = req.params.id;
+        films.deleteOne({ _id: id })
+            .then(() => {
+                res.redirect('back');
             })
             .catch(next)
     }
--- film/src/routes/delete.js
+++ film/src/routes/delete.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const deleteController = require('../app/controllers/DeleteController.js')
-
+router.post('/store/:id', deleteController.deleteByID);
 router.get('/', deleteController.delete);
 router.post('/', deleteController.delete_post)
 
